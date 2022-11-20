544e15ad8e61d2808d941018517d90c89d3376c5
Ernie Gurish <gurish.e@northeastern.edu>
2022-11-19 22:04:24 -0500

    Add dislikes to shown stats and switch likes to thumbs up

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit}) => {
+const TuitStats = ({tuit, likeTuit, dislikeTuit}) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -13,9 +13,18 @@ const TuitStats = ({tuit, likeTuit}) => {
         </div>
         <div className="col">
             <span onClick={() => likeTuit(tuit)}>
-              {tuit.stats.likes > 0 && <i className="fas fa-heart" style={{color: 'red'}}></i>}
-              {tuit.stats.likes <= 0 && <i className="far fa-heart"></i>}
+              {tuit.stats.likes > 0 &&
+                  <i className="fa-solid fa-thumbs-up" style={{color: 'mediumblue'}}></i>
+              }
+              {tuit.stats.likes <= 0 && <i className="fa-solid fa-thumbs-up"></i>}
               {tuit.stats && tuit.stats.likes}
+            </span>
+            <span onClick={() => dislikeTuit(tuit)}>
+                {tuit.stats.dislikes > 0 &&
+                    <i className="fa-solid fa-thumbs-down" style={{color: 'red'}}></i>
+                }
+                {tuit.stats.dislikes <= 0 && <i className="fa-solid fa-thumbs-down"></i>}
+                {tuit.stats && tuit.stats.dislikes}
             </span>
         </div>
         <div className="col">
